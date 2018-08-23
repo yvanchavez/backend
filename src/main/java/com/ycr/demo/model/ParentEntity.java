8f725522826d13a939a6125b38a76e1c9626e91c
¨Yvan <chavez.yvan@gmail.com>
2018-08-23 17:25:30 -0500

    EntityUser created

---
+++ src/main/java/com/ycr/demo/model/ParentEntity.java
@@ -0,0 +1,35 @@
+package com.ycr.demo.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+
+@MappedSuperclass
+@Access(AccessType.FIELD)
+public class ParentEntity implements Serializable{
+
+	private static final long serialVersionUID = 7955333246761084221L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="id",unique=true,nullable=false)
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	
+
+}
